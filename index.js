23444afeaa43d0c51778b232f02bd475f98908ad
Abayraj <abayunni5@gmail.com>
2023-11-13 01:21:15 +0530

    added git ignore file

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("")
+app.post("")
+app.put("")
+app.delete("")
+
+
+
+
+
+
+const PORT = 4005;
+
+app.listen(PORT,()=> console.log(`server running on ${PORT}`));
